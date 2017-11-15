401442433d43c2af22ca44cd6ccb4c29de2e31f1
jeremiarm <jeremiarm5@gmail.com>
2017-11-15 13:56:35 +0700

    perbaikan nomor 3 dan 4, template dasar

--- fuse2.c
+++ fuse2.c
@@ -1,11 +1,4 @@
 #define FUSE_USE_VERSION 28
-#define HAVE_SETXATTR
-#ifdef HAVE_CONFIG_H
-#include <config.h>
-#endif
-#ifdef linux
-#define _XOPEN_SOURCE 500
-#endif
 #include <fuse.h>
 #include <stdio.h>
 #include <string.h>
@@ -14,20 +7,194 @@
 #include <dirent.h>
 #include <errno.h>
 #include <sys/time.h>
-#ifdef HAVE_SETXATTR
-#include <sys/xattr.h>
-#endif
 
+static const char *dirpath = "/home/eplayer/Downloads";
+
+static int xmp_getattr(const char *path, struct stat *stbuf)
+{
+ 	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = lstat(fpath, stbuf);
+
+	if (res == -1)
+		return -errno;
+
+	return 0;
+}
+
+static int xmp_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+		       off_t offset, struct fuse_file_info *fi)
+{
+ 	char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+
+	DIR *dp;
+	struct dirent *de;
+
+	(void) offset;
+	(void) fi;
+
+	dp = opendir(fpath);
+	if (dp == NULL)
+		return -errno;
+
+	while ((de = readdir(dp)) != NULL) {
+		struct stat st;
+		memset(&st, 0, sizeof(st));
+		st.st_ino = de->d_ino;
+		st.st_mode = de->d_type << 12;
+		res = (filler(buf, de->d_name, &st, 0));
+			if(res!=0) break;
+	}
+
+	closedir(dp);
+	return 0;
+}
+
+static int xmp_read(const char *path, char *buf, size_t size, off_t offset,
+		    struct fuse_file_info *fi)
+{
+ 	char fpath[1000];
+	if(strcmp(path,"/") == 0)
+	{
+		path=dirpath;
+		sprintf(fpath,"%s",path);
+	}
+	else sprintf(fpath, "%s%s",dirpath,path);
+	int res = 0;
+ 	int fd = 0 ;
+
+	(void) fi;
+	fd = open(fpath, O_RDONLY);
+	if (fd == -1)
+		return -errno;
+
+	res = pread(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+
+	close(fd);
+	return res;
+}
+
+static int xmp_rename(const char *from, const char *to)
+{
+	int res;
+	res = rename(from, to);
+	if (res == -1)
+	return -errno;
+	return 0;
+}
+
+static int xmp_mknod(const char *path, mode_t mode, dev_t rdev)
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	if (S_ISREG(mode)) {
+		res = open(fpath, O_CREAT | O_EXCL | O_WRONLY, mode);
+		if (res >= 0)
+		res = close(res);
+	} else if (S_ISFIFO(mode))
+	res = mkfifo(fpath, mode);
+	else
+	res = mknod(fpath, mode, rdev);
+	if (res == -1)
+	return -errno;
+	return 0;
+}
+
+static int xmp_open(const char *path, struct fuse_file_info *fi)
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = open(fpath, fi->flags);
+	if (res == -1)
+		return -errno;
+	close(res);
+	return 0;
+}
+
+static int xmp_truncate(const char *path, off_t size)
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = truncate(fpath, size);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+
+static int xmp_chmod(const char *path, mode_t mode)
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	res = chmod(fpath, mode);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+
+static int xmp_utimens(const char *path, const struct timespec ts[2])
+{
+	int res;
+	char fpath[1000];
+	sprintf(fpath,"%s%s",dirpath,path);
+	struct timeval tv[2];
+	tv[0].tv_sec = ts[0].tv_sec;
+	tv[0].tv_usec = ts[0].tv_nsec / 1000;
+	tv[1].tv_sec = ts[1].tv_sec;
+	tv[1].tv_usec = ts[1].tv_nsec / 1000;
+	res = utimes(fpath, tv);
+	if (res == -1)
+		return -errno;
+	return 0;
+}
+
+static int xmp_write(const char *path, const char *buf, size_t size,
+off_t offset, struct fuse_file_info *fi)
+{
+	int fd;
+	int res;
+	char fpath[1000];
+	(void) fi;
+	sprintf(fpath,"%s%s",dirpath,path);
+	fd = open(fpath, O_WRONLY);
+	if (fd == -1)
+		return -errno;
+	res = pwrite(fd, buf, size, offset);
+	if (res == -1)
+		res = -errno;
+	close(fd);
+	return res;
+}
 
 static struct fuse_operations xmp_oper = {
-.getattr	= xmp_getattr,
-.write		= xmp_write,
-.readlink	= xmp_readlink,
-.readdir	= xmp_readdir,
-.mknod		= xmp_mknod,
-.rename		= xmp_rename,
-.chmod		= xmp_chmod,
-.truncate	= xmp_truncate,
-.utimens	= xmp_utimens,
-.open		= xmp_open,
+	.getattr	= xmp_getattr,
+	.readdir	= xmp_readdir,
+	.read		= xmp_read,
+	.write		= xmp_write,
+	.rename		= xmp_rename,
+	.mknod		= xmp_mknod,
+	.open		= xmp_open,
+	.chmod		= xmp_chmod,
+	.truncate	= xmp_truncate,
+	.utimens	= xmp_utimens,
 };
+
+int main(int argc, char *argv[])
+{
+	umask(0);
+	return fuse_main(argc, argv, &xmp_oper, NULL);
+}
+
